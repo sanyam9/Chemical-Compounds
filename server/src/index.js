87c0b153ecffa96e042f32d0748c1281f695e54a
Sanyam <sanyamxsharma@gmail.com>
2023-07-21 21:26:29 +0530

    chore: add cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router  = require('./routes');
 const { PORT } = require('./config');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
